f856629ba0b80644b25e1c458ef8a949f610176e
Claudia Gillota <cgillota@gmail.com>
2023-04-03 23:06:23 -0400

    working on js

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ```
 AS A traveler
 I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
+SO THAT I can plan a trip accordingly.
 ```
 
 ## Acceptance Criteria
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-var APIKey = "656e6c8ef75718035fce76f2d435d117";
+//This is more so for me
+const APIKey = "656e6c8ef75718035fce76f2d435d117";
+const form = document.querySelector(".card-deck");
 var city; 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey; 
-fetch(queryURL)
+fetch(queryURL) 
+//
+
+$(function () {
+    $("#search-button").on("click", function(){
+        var city = $("#city").val();
+    }) 
+})
+
+var getItems = function () {
+    var storedCities = JSON.parse(localStorage.getItem("search-history"));
+}
+ var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid" + "656e6c8ef75718035fce76f2d435d117";
+ fetch(queryURL)
+ .then(function (response){ 
+    return response.json();
+ })
